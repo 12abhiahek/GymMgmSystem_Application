403dc39aef3a9e80ee6792b21b3da443f8039ec2
abhishek singh <abhisheksingh@Mac.lan>
2026-07-16 14:17:42 +0530

    gym_sys_Application_setup_security

---
+++ src/main/java/com/gym/gymmgmsys/GymMgmSysApplication.java
@@ -0,0 +1,13 @@
+package com.gym.gymmgmsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymMgmSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymMgmSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gym/gymmgmsys/GymMgmSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.gymmgmsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymMgmSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
